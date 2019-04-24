2339e81de695e524c82b9da81da52313f66d609f
Jaroslav Šmolík <grissius@gmail.com>
2019-04-24 08:55:06 +0200

    Fix docs build

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -3,18 +3,18 @@ import { ConfigLoaderOptions } from './loader';
 const fromPairs = (pairs: Array<[keyof any, any]>) => Object.assign({}, ...Array.from(pairs, ([k, v]) => ({ [k]: v })));
 const { keys } = Object;
 
-export const createConfigStorage = (opts: Pick<ConfigLoaderOptions, 'defaultConfigPath'|'userConfigPath'|'envMode'>): Record<any, any> => {
+export const createConfigStorage = (
+    opts: Pick<ConfigLoaderOptions, 'defaultConfigPath' | 'userConfigPath' | 'envMode'>
+): Record<any, any> => {
     const defaultConfig = opts.defaultConfigPath ? JSON.parse(readFileSync(opts.defaultConfigPath, 'utf-8')) : {};
     const userConfig = opts.userConfigPath ? JSON.parse(readFileSync(opts.userConfigPath, 'utf-8')) : {};
-    let envConfig = {};
-    if (opts.envMode === 'default') {
-        envConfig = opts.envMode ? fromPairs(keys(defaultConfig).map(k => [k, process.env[k]])) : {};
-    } else if (opts.envMode === 'merged') {
-        envConfig = opts.envMode ? fromPairs([...keys(defaultConfig), ...keys(userConfig)].map(k => [k, process.env[k]])) : {};
+    let envConfig: any = {};
+    if (opts.envMode === 'default' || opts.envMode === 'merged') {
+        const configKeys = [...keys(defaultConfig), ...(opts.envMode === 'merged' ? keys(userConfig) : [])];
+        envConfig = fromPairs(configKeys.map(k => [k, process.env[k]] as [string, any])) as any;
     } else if (opts.envMode === 'all') {
         envConfig = process.env;
     }
-    envConfig = opts.envMode ? fromPairs(keys(defaultConfig).map(k => [k, process.env[k]])) : {};
     const configStorage = { ...defaultConfig, ...userConfig, ...envConfig };
     return configStorage;
 };
