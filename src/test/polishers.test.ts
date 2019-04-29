06f3a1d38ac4532b20e4fd063d936fdabee6867f
Jaroslav Šmolík <grissius@gmail.com>
2019-04-29 09:56:20 +0200

    Add type tests

--- src/test/polishers.test.ts
+++ src/test/polishers.test.ts
@@ -1,5 +1,30 @@
+import { inspectWithPreamble } from 'intspector';
 import { createAtomLoaderFactory } from 'lib/loader';
 import { values } from 'lib/polishers';
+
+const typeTest = inspectWithPreamble(`
+import { createAtomLoaderFactory } from 'lib/loader';
+import { values } from 'lib/polishers';
+
+const string = createAtomLoaderFactory({
+    FOO: 'foo',
+    BAR: 'bar',
+    BAZ: 'baz',
+})(String);
+
+const config1 = values({
+    my: {
+        deep: {
+            poem: string('FOO'),
+        },
+    },
+    bar: string('BAR'),
+    withConstant: {
+        baz: string('BAZ'),
+        ans: 42,
+    },
+})
+`);
 
 const string = createAtomLoaderFactory({
     FOO: 'foo',
@@ -24,6 +49,11 @@ describe('polishers', () => {
                     },
                 })
             ).toMatchSnapshot();
+        });
+        test('types', () => {
+            expect(typeTest('typeof config1')).toMatchInlineSnapshot(
+                '"{ my: { deep: { poem: string; }; }; bar: string; withConstant: { baz: string; ans: number; }; }"'
+            );
         });
     });
 });
