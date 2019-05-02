6f7bffb6673feb1054cdcb54dfc627be7f1d5ea6
Jiri Smolik <jiri.smolik@ackee.cz>
2019-05-02 08:59:01 +0200

    Throw a descriptive error on invalid config file

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,13 +1,22 @@
 import { readFileSync } from 'fs';
+import * as path from 'path';
 import { ConfigLoaderOptions } from './loader';
 const fromPairs = (pairs: Array<[keyof any, any]>) => Object.assign({}, ...Array.from(pairs, ([k, v]) => ({ [k]: v })));
 const { keys } = Object;
 
+const loadFile = (filePath?: string) => {
+    try {
+        return filePath ? JSON.parse(readFileSync(filePath, 'utf-8')) : {};
+    } catch (error) {
+        throw new Error(`Missing or invalid config file \`${filePath}\` at ${path.join(path.resolve('.'), filePath!)}`);
+    }
+};
+
 export const createConfigStorage = (
     opts: Pick<ConfigLoaderOptions, 'defaultConfigPath' | 'userConfigPath' | 'envMode'>
 ): Record<any, any> => {
-    const defaultConfig = opts.defaultConfigPath ? JSON.parse(readFileSync(opts.defaultConfigPath, 'utf-8')) : {};
-    const userConfig = opts.userConfigPath ? JSON.parse(readFileSync(opts.userConfigPath, 'utf-8')) : {};
+    const defaultConfig = loadFile(opts.defaultConfigPath);
+    const userConfig = loadFile(opts.userConfigPath);
     let envConfig: any = {};
     if (opts.envMode === 'default' || opts.envMode === 'merged') {
         const configKeys = [...keys(defaultConfig), ...(opts.envMode === 'merged' ? keys(userConfig) : [])];
