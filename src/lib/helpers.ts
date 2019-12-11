a12de88f4a4530de1bb05ad850d5d95fe3fc70ab
Jiří Šmolík <jiri.smolik@ackee.cz>
2019-12-11 16:05:36 +0100

    ✨ Add jsonc helper

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,5 @@
+import * as jsonParser from 'jsonc-parser';
+
 export const anonymize = (val: any) => {
     const str = String(val);
     const show = Math.min(str.length / 6, 10);
@@ -8,3 +10,14 @@ export const parseBool = (x: any) => (x === 'false' || x === '0' ? false : Boole
 export const identity = <T>(x: T) => x;
 
 export const isObject = (x: any) => typeof x === 'object' && Object.prototype.toString.call(x) === '[object Object]';
+
+export const JSONC = {
+    parse: (text: string) => {
+        const errors: jsonParser.ParseError[] = [];
+        const parsed = jsonParser.parse(text, errors);
+        if (errors.length) {
+            throw new SyntaxError(`${jsonParser.printParseErrorCode(errors[0].error)} at position ${errors[0].offset}`);
+        }
+        return parsed;
+    },
+};
