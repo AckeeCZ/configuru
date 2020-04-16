422fe997bec9a4743311cbfe5eee8656fc503745
Stefan Prokop <sprokop1010@seznam.cz>
2020-04-16 11:44:24 +0200

    ✨ Make the whole string hidden after anonymize

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,10 +1,6 @@
 import * as jsonParser from 'jsonc-parser';
 
-export const anonymize = (val: any) => {
-    const str = String(val);
-    const show = Math.min(str.length / 6, 10);
-    return [str.slice(0, show), '***', str.slice(str.length + 1 - show, str.length)].join('');
-};
+export const anonymize = (val: any) => (val === '' ? val : '[redacted]');
 
 export const parseBool = (x: any) => (x === 'false' || x === '0' ? false : Boolean(x));
 export const identity = <T>(x: T) => x;
